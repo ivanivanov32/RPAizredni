4b1002ddbd52d6d69bba2a6817ffccf2fee4401e
ivan Ivanov <inf.0912@scng.si>
2023-01-18 17:02:52 +0100

    prvi commit za test

---
+++ prvavaja/prvavaja/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prvavaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            console.writeline(pozdravljen svet)
+            console.readline()
+        {
+        }
+    }
+}
